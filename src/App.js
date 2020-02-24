bd49e350681d48baf9e90555b2ee33d8b0f44eef
harootyoon <harutyuna97@gmail.com>
2020-02-24 17:41:55 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,94 @@
 import React, {Component} from 'react';
 
 class App extends Component{
+  state = {
+    inputValue: '',
+    inputNewValue: '',
+    inputOperator: '',
+  };
+
+  handleClick = (e) => {
+    this.setState({
+      inputValue: this.state.inputValue + e.target.id
+    })
+  };
+
+  handleClickZero = (e) => {
+    if (this.state.inputValue !== '') {
+      this.setState({
+        inputValue: this.state.inputValue + e.target.id
+      })
+    }
+  };
+
+  handleClear = () => {
+    this.setState({
+      inputValue: '',
+      inputNewValue: '',
+      inputOperator: '',
+    })
+  };
+
+  handleOperator = (e) => {
+    this.setState({
+      inputNewValue: this.state.inputValue,
+      inputValue: '',
+      inputOperator: e.target.id
+    });
+  };
+
+  handleEqual = () => {
+      switch (this.state.inputOperator) {
+        case "plus":
+          this.setState({
+            inputValue: +this.state.inputNewValue + +this.state.inputValue
+          });
+              break;
+        case "times":
+          this.setState({
+            inputValue: +this.state.inputNewValue * +this.state.inputValue
+          });
+              break;
+        case "divide":
+          this.setState({
+            inputValue: +this.state.inputNewValue / +this.state.inputValue
+          });
+              break;
+        case "minus":
+          this.setState({
+            inputValue: +this.state.inputNewValue - +this.state.inputValue
+          });
+              break;
+        default:
+          return null
+      }
+  };
+
   render() {
     return (
-        <div>
-          
+        <div className='mainDiv'>
+          <div>
+            <input value={this.state.inputValue} readOnly className = 'calc-input'/>
+          </div>
+          <div className='calc-main'>
+            <button onClick = {this.handleOperator} className = 'itemX' id = 'times'> X </button>
+            <button onClick = {this.handleEqual} className = 'itemEqual'> = </button>
+            <button onClick = {this.handleOperator} className = 'itemDivide' id = 'divide'> / </button>
+            <button onClick = {this.handleClear} className = 'itemC'> C </button>
+            <button onClick = {this.handleClick} className = 'item1' id = '1'> 1 </button>
+            <button onClick = {this.handleClick} className = 'item2' id = '2'> 2 </button>
+            <button onClick = {this.handleClick} className = 'item3' id = '3'> 3 </button>
+            <button onClick = {this.handleOperator} className = 'itemPlus' id = 'plus'> + </button>
+            <button onClick = {this.handleClick} className = 'item4' id = '4'> 4 </button>
+            <button onClick = {this.handleClick} className = 'item5' id = '5'> 5 </button>
+            <button onClick = {this.handleClick} className = 'item6' id = '6'> 6 </button>
+            <button onClick = {this.handleOperator} className = 'itemMinus' id = 'minus'> - </button>
+            <button onClick = {this.handleClick} className = 'item7' id = '7'> 7 </button>
+            <button onClick = {this.handleClick} className = 'item8' id = '8'> 8 </button>
+            <button onClick = {this.handleClick} className = 'item9' id = '9'> 9 </button>
+            <button onClick = {this.handleClickZero} className = 'item0' id = '0'> 0 </button>
+            <button onClick = {this.handleClick} className = 'itemDot' id = '.'> . </button>
+          </div>
         </div>
     )
   }
